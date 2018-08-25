0f37da402c3c42952b56ee7b5c6c0aa31944afeb
LuisOjedaC <luis.ojeda@usantoto.edu.co>
2018-08-24 21:10:46 -0500

    Initial commit

---
+++ app/src/main/java/diego/app/diego/com/grados/MainActivity.kt
@@ -0,0 +1,12 @@
+package diego.app.diego.com.grados
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
